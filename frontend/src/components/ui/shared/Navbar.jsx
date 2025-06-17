88fd1e92a5584d90e79d0ad9e1b09b529de5e679
sarahfatima1205 <142889410+sarahfatima1205@users.noreply.github.com>
2025-06-17 12:19:41 +0530

    adding frontend setup

---
+++ frontend/src/components/ui/shared/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar=()=>{
+    return(
+        <div className='bg-white'>
+            <h1>Job<span>Portal</span></h1>
+        </div>
+    )
+}
+export default App;
